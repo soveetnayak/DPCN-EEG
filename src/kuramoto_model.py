7735701bcd78bff9632fa131ac3a64f8025adba6
Soveet Nayak <soveet.nayak@gmail.com>
2024-04-12 13:25:51 +0530

    figures

--- src/kuramoto_model.py
+++ src/kuramoto_model.py
@@ -14,11 +14,6 @@ import pandas as pd
 from tqdm import tqdm
 from scipy.integrate import odeint
 
-df = pd.read_csv("../data/111g0L_filtered_fragment_0_binary.csv", header=None)
-A = df.values
-print(A)
-adjMatrix = np.array(A)
-
 class Kuramoto:
     def __init__(self, coupling, dt, T, n_nodes):
         self.dt = dt
@@ -49,42 +44,31 @@ class Kuramoto:
         suma = sum([(np.e ** (1j * i)) for i in angles_vec])
         return abs(suma / len(angles_vec))
 
-model = Kuramoto(coupling=3, dt=0.01, T=1000, n_nodes=len(adjMatrix))
-x = model.run(adjMatrix)
-print(x.shape)
+df = pd.read_csv("../data/111g0L_filtered_fragment_0_binary.csv", header=None)
+A = df.values
+print(A)
+adjMatrix = np.array(A)
+n = len(adjMatrix)
+
+# model = Kuramoto(coupling=3, dt=0.01, T=1000, n_nodes=n)
+# x = model.run(adjMatrix)
+# print(x.shape)
 
 # Run model with different coupling (K) parameters
 coupling_vals = np.linspace(0, 3, 500)
 runs = []
-n = len(adjMatrix)
 for coupling in tqdm(coupling_vals):
-    model = Kuramoto(coupling=coupling, dt=0.01, T=1000, n_nodes=n)
+    model = Kuramoto(coupling=coupling, dt=0.1, T=100, n_nodes=n)
     model.natfreqs = np.random.normal(1, 0.4, size=n)  # reset natural frequencies
     act_mat = model.run(adjMatrix)
     runs.append(act_mat)
 
 runs_array = np.array(runs)
 
-# Plot final Rt for each coupling value
+''' 
 plt.figure()
 for i, coupling in tqdm(enumerate(coupling_vals)):
-    r_mean = np.mean([model.phase_coherence(vec)
-                      for vec in runs_array[i, :, -1000:].T]) # mean over last 1000 steps
-    plt.scatter(coupling, r_mean, c='blue', s=20, alpha=0.7)
-
-# Predicted Kc – analytical result (from paper)
-Kc = np.sqrt(8 / np.pi) * np.std(model.natfreqs) # analytical result (from paper)
-plt.vlines(Kc, 0, 1, linestyles='--', color='orange', label='analytical prediction')
-
-plt.legend()
-plt.grid(linestyle='--', alpha=0.8)
-plt.ylabel('order parameter (R)')
-plt.xlabel('coupling (K)')
-sns.despine()
-
-plt.figure()
-for i, coupling in tqdm(enumerate(coupling_vals)):
-    r_values = [model.phase_coherence(vec) for vec in runs_array[i, :, -1000:].T]
+    r_values = [model.phase_coherence(vec) for vec in runs_array[i, :, -80000:].T]
     r_mean = np.mean(r_values)
     r_std = np.std(r_values)
     plt.errorbar(coupling, r_mean, yerr=r_std, fmt='o', c='yellow', alpha=0.7)
@@ -96,20 +80,23 @@ plt.vlines(Kc, 0, 1, linestyles='--', color='orange', label='analytical predicti
 
 plt.legend()
 plt.grid(linestyle='--', alpha=0.8)
-plt.ylabel('order parameter (R)')
-plt.xlabel('coupling (K)')
+plt.ylabel('Order Parameter (R)')
+plt.xlabel('Coupling (K)')
 sns.despine()
-plt.show()
+# plt.show()
+# plt.savefig('../figures/11g0L_0_R_vs_K.png')
+'''
 
 mean_phase_coherences = []
-
+std_phase_coherences = []
 for i, coupling in tqdm(enumerate(coupling_vals)):
-    r_mean = np.mean([model.phase_coherence(vec)
-                      for vec in runs_array[i, :, -1000:].T]) # mean over last 1000 steps
+    # mean over 80k steps
+    r_mean = np.mean([model.phase_coherence(vec) for vec in runs_array[i, :, -1000:].T])
     mean_phase_coherences.append(r_mean)
+    r_std = np.std([model.phase_coherence(vec) for vec in runs_array[i, :, -1000:].T])
+    std_phase_coherences.append(r_std)
 
 # Plot mean phase coherence curve
-
 plt.figure()
 Kc = np.sqrt(8 / np.pi) * np.std(model.natfreqs) # analytical result (from paper)
 plt.vlines(Kc, 0, 1, linestyles='--', color='orange', label='analytical prediction')
@@ -120,3 +107,19 @@ plt.title('Mean Phase Coherence vs Coupling Strength')
 plt.legend()
 plt.grid(True)
 plt.show()
+# plt.savefig('../figures/11g0L_0_Mean_Phase_Coherence_vs_K.png')
+
+'''
+# Plot the line for mean phase coherence with standard deviation as shaded region
+plt.figure()
+plt.plot(coupling_vals, mean_phase_coherences, color='blue', label='Mean Phase Coherence')
+plt.fill_between(coupling_vals, np.array(mean_phase_coherences) - np.array(std_phase_coherences),
+                    np.array(mean_phase_coherences) + np.array(std_phase_coherences), color='blue', alpha=0.3)
+plt.xlabel('Coupling (K)')
+plt.ylabel('Mean Phase Coherence')
+plt.title('Mean Phase Coherence vs Coupling Strength')
+plt.legend()
+plt.grid(True)
+plt.show() 
+# plt.savefig('../figures/11g0L_0_Mean_Phase_Coherence_with_std_vs_K.png')
+'''
